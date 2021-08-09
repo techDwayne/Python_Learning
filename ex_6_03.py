88dcb86832cdff575c01f387c605abafb8d3e1b3
techD <ldsudduth@live.com>
2021-08-08 22:10:11 -0400

    Update ex_6_03.py

--- ex_6_03.py
+++ ex_6_03.py
@@ -1,9 +1,9 @@
 # strings ex_6_03
-word=input("Input a word ")
+winp=input("Input a word ")
 linp=input("Input a letter in the word ")
 def str_counter():
     ct = 0
-    for letter in word:
+    for letter in winp:
         if letter == linp:
             ct=ct+1
     print("Total Letter Count:", ct)
