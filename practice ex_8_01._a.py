662372e595309b29067d9a468be83b5162e459c2
techD <ldsudduth@live.com>
2021-08-16 18:07:38 -0400

    new exercises

---
+++ practice ex_8_01._a.py
@@ -0,0 +1,14 @@
+numlist=list()
+while True:
+    inp=input('Enter a number: ', )
+    if inp=='done': break
+    value=float(inp)
+    numlist.append(value)
+average=sum(numlist)/len(numlist)
+mx=max(numlist)
+mn=min(numlist)
+total=sum(numlist)
+print('Average: ', average)
+print('Maximum: ', mx)
+print('Minimum: ', mn)
+print('Total: ', total )
