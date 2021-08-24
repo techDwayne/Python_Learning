0f9a2526d9db2a83a2a80c4f47407305fcbf84c0
techD <ldsudduth@live.com>
2021-08-24 17:22:35 -0400

    Update practice_ex_9.02a.py

--- practice_ex_9.02a.py
+++ practice_ex_9.02a.py
@@ -1,1 +1,21 @@
 #practice_ex_9.02a.py
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+counts = dict()
+for line in fhand:
+    words = line.split()
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+print(counts)
+lst=list(counts.keys())
+#print(lst)
+lst.sort()
+for key in lst:
+    print(key,counts[key])
