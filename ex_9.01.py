d59160ef73a033b25d5c881d8087d9c575d06295
techD <ldsudduth@live.com>
2021-08-24 21:53:19 -0400

    Create ex_9.01.py

---
+++ ex_9.01.py
@@ -0,0 +1,15 @@
+#practice_ex_9.03.py
+name=input('Enter File Name: ')
+handle=open(name)
+counts=dict()
+for line in  handle:
+    words=line.split()
+    for word in words[]:
+        counts[word] = counts.get(word,0)+1
+bigcount=None
+bigword=None
+for word, count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword=word
+        bigcount=count
+print(bigword, bigcount) 
