dee2f6cb8c0ea01393f437c3ebe1913faa570aec
techD <ldsudduth@live.com>
2022-06-23 06:39:51 -0400

    fixed it!

---
+++ ITProTV/Lesson2_Lab.py
@@ -0,0 +1,15 @@
+#Declare multiple variables with the same value
+p=t=q=1024
+print('The value of p is: ', p)
+print('The value of t is: ', t)
+print('The value of q is: ', q)
+
+#Declare multiple variable with different values on the same line
+j,u,p,i,t,e,r="Big", 1024, 517.97, "Planet", 80,90, 97
+print('The value of j is: ',j)
+print('The value of u is: ',u)
+print('The value of p is: ',p)
+print('The value of i is: ',i)
+print('The value of t is: ',t)
+print('The value of e is: ',e)
+print('The value of r is: ',r)
