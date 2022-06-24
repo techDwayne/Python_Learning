d4f11723531cbbe6356529a04132a18dac52b974
techD <ldsudduth@live.com>
2022-06-24 13:45:26 -0400

    commit

---
+++ ITProTV/Lesson4_Lab.py
@@ -0,0 +1,12 @@
+#Create a list of student name
+Student_list = ['John', 'Smith', 'Kathy', 'Philip']
+#call a list
+print(Student_list)
+#print the first item of the list
+print(Student_list[0])
+#print the second item of the list
+print(Student_list[1])
+#print the thirdd item of the list
+print(Student_list[2])
+#print the fourth item of the list
+print(Student_list[3])
--- ITProTV/Lesson4_Lists.py
+++ ITProTV/Lesson4_Lists.py
@@ -1,3 +1,19 @@
 ## our first list
 edutainers = ['Adam', 'Aubri', 'Daniel', 'Jo', 'Justin', 'Zach']
 print("Edutainers: ", edutainers)
+print('The third edutainter is: ', edutainers[2])
+#How do I know how many elements there are programatically
+print('The number of edutainers is: ', len(edutainers))
+#use with string
+print('Justin has ',len('Justin'), 'characters in his name.')
+#or use element position
+print('Justin has ',len(edutainers[4]), 'characters in their name.')
+
+#How do I convert a string (others can apply) to a list?
+favorite_food='fish taco'
+favorite_food_character_list=list(favorite_food)
+print('Favorite Food: ', favorite_food_character_list)
+print('Characters: ', favorite_food)
+
+
+
