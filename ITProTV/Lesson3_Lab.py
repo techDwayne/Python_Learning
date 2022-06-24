1b1d54cb3ee1b16f30f323e0688483d2f3c8cb1a
techD <ldsudduth@live.com>
2022-06-24 09:46:40 -0400

    commet

---
+++ ITProTV/Lesson3_Lab.py
@@ -0,0 +1,33 @@
+#use the type() function to identify the data type of variable 'a'
+a=12
+print('The data type of variable a is: ', type(a))
+#use the type() function to identry the data type of variable 'b'
+b='Galaxy'
+print("the data type of variable  b is:", type(b))
+#use the type() function to identify the data type of variable 'c'
+c="Fruit"
+print("The data type of variable 'c' is:", type(c))
+#use the type() function to identify the data type of variable 'd'
+d=12.45
+print("The data type of variable 'd' is: ", type(d))
+#Convert a string variable to an integer and perform addtion
+e='6537'
+f=10
+g=int(e)+f
+print('The sum of e and f is: ', g)
+#convert a string variable to a float type variable and perform addtion
+h='5537'
+i=10.463
+j=float(h)+1
+print('The sum of h and i is: ', j)
+#Convert an integer variable to a float type variable
+k=872
+print('The converted floating point value of k is: ', float(k))
+#convert float type variables to integer type variables and perform addition
+l=1834.56
+m=1294.68
+n=int(l)+int(m)
+print('The sum of l and m is:', n)
+#Convert an integer variable to a character type
+o=100
+print('The character value of the number 100 is: ', o)
