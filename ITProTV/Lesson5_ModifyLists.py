8928c0e13e2305a6e915d79483b074e542287175
techD <ldsudduth@live.com>
2022-06-27 13:44:52 -0400

    commit

---
+++ ITProTV/Lesson5_ModifyLists.py
@@ -0,0 +1,24 @@
+edutainers=['Adam', 'Aubri', 'Daniel', 'Jo']
+print('Edutainters: ', edutainers)
+#How do I add an element to the end
+edutainers.append('Justin')  #Method
+print('Edutainers: ', edutainers)
+edutainers.reverse()
+print('Edutatiners:', edutainers)
+edutainers.reverse()
+
+#How do I remove an element from a list
+#del edutainers [3]
+#print('Edutainers: ', edutainers)
+
+#How do I remove an element from the end of a list
+special = edutainers.pop()
+print('Special:', special)
+
+#How do I change an element in a list
+edutainers[2]='Dan'
+print('Edutainers: ', edutainers)
+
+edutainers.sort()
+print('Edutainters:', edutainers)
+
