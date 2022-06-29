57a6523bdbbbc8feaf7082c4677001bf22ab09d6
techD <ldsudduth@live.com>
2022-06-29 08:16:19 -0400

    commeit

---
+++ ExperimentationFolder/Lists.py
@@ -0,0 +1,9 @@
+from numpy import true_divide
+
+
+my_TV=['FBI', 'FBI International', 'FBI Most Wanted', 'Chicago Med', 'Chicago Fire', 'NCIS New Orleans', 'NCIS', 'Equalizer', 'Star Trek: Strange New Worlds']
+print(my_TV)
+print(my_TV[4])
+sorted(my_TV)
+print(my_TV)
+print(my_TV[4])
