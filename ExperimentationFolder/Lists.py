39323e369f9c649a1f9167fdd5e00ff279f2111e
techD <ldsudduth@live.com>
2022-06-29 10:22:42 -0400

    commit

--- ExperimentationFolder/Lists.py
+++ ExperimentationFolder/Lists.py
@@ -1,9 +1,12 @@
-from numpy import true_divide
-
-
 my_TV=['FBI', 'FBI International', 'FBI Most Wanted', 'Chicago Med', 'Chicago Fire', 'NCIS New Orleans', 'NCIS', 'Equalizer', 'Star Trek: Strange New Worlds']
 print(my_TV)
 print(my_TV[4])
-sorted(my_TV)
+srt_myTV=sorted(my_TV)
+print(srt_myTV)
+print(srt_myTV[4])
+my_TV.sort(reverse=True)
+print(my_TV)
+print(my_TV[4])
+my_TV.reverse()
 print(my_TV)
 print(my_TV[4])
