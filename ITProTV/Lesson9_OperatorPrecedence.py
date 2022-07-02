be6877727cf87d4d464c1d33b23a4cd22fa34cd6
techD <ldsudduth@live.com>
2022-07-02 17:01:43 -0400

    commit

---
+++ ITProTV/Lesson9_OperatorPrecedence.py
@@ -0,0 +1,8 @@
+#membership tests
+edutainers=['Vonne', 'Justin', 'Aubri', 'Ronnie']
+print('Daniel in Edutainers: ', 'Daniel'in edutainers)
+print('Vonne in Edutainers: ', 'Vonne'in edutainers)
+print('Daniel in Edutainers: ', 'Daniel' not in edutainers)
+
+#Identity Tests
+
