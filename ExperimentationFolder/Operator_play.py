eb5ce81f279c51ecf46bf312abf2735d3c885fb2
techD <ldsudduth@live.com>
2022-07-03 06:29:03 -0400

    commit

--- ExperimentationFolder/Operator_play.py
+++ ExperimentationFolder/Operator_play.py
@@ -23,3 +23,21 @@ names_1=["Liam",
     "Mason",]
 
 print('Names are equal to names_1:', names in names_1)
+
+i=20
+h=10
+j=h*i
+k=i/h
+l=i*h
+m=i+h
+n=i-h
+o=i<h
+p=i==h
+q=i>h
+r=i!=h
+s=i<=h
+t=i>=h
+print(j,k,l,m,n,o,p,q,r,s,t,)
+print(i+h*(j-i)/m)#PEMDAS
+
+
