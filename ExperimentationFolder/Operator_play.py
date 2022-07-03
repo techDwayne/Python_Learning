3baeb022481a0bc129c0d739cfe089a1e479b7b9
techD <ldsudduth@live.com>
2022-07-03 06:17:44 -0400

    commit

---
+++ ExperimentationFolder/Operator_play.py
@@ -0,0 +1,25 @@
+names=["Liam",
+    "Noah",
+    "Oliver",
+    "Elijah",
+    "William",
+    "James",
+    "Benjamin",
+    "Lucas",
+    "Henry",
+    "Alexander",
+    "Mason",]
+
+names_1=["Liam",
+    "Noah",
+    "Oliver",
+    "Elijah",
+    "William",
+    "James",
+    "Benjamin",
+    "Lucas",
+    "Henry",
+    "Alexander",
+    "Mason",]
+
+print('Names are equal to names_1:', names in names_1)
