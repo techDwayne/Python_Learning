19333eaec7fcb1320fe1668a389f6852a69ee4a9
techD <ldsudduth@live.com>
2022-07-04 14:01:56 -0400

    commit

---
+++ ExperimentationFolder/Age100.py
@@ -0,0 +1,12 @@
+#get name
+name=input('Enter your name: ')
+print('Hi, ', name)
+age=int(input('Enter your age: '))
+y = 2022-age+100
+print('You will turn 100 in: ', y)
+reprint =int(input('How many times to reprint? '))
+while reprint>0:
+    print('You will turn 100 in', y)
+    reprint=reprint-1;
+else:
+    print('Done.')
