94e2835b95f557149f1d38b745f4d66905b13c6b
techD <ldsudduth@live.com>
2022-07-04 13:13:02 -0400

    commit

---
+++ ExperimentationFolder/odd_or_even.py
@@ -0,0 +1,11 @@
+#odd or even
+
+x = int(input('Number: '))
+if (x%2)==0:
+    if(x%4)==0:
+        print('Even Number divisbile by 4!')
+    else:
+        print('Even Number!')
+else:
+    print('Odd Number')
+    
