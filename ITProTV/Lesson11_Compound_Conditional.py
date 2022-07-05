d976c422b1713455c1b775b446dd2e46869a8950
techD <ldsudduth@live.com>
2022-07-05 14:43:07 -0400

    commit

--- ITProTV/Lesson11_Compound_Conditional.py
+++ ITProTV/Lesson11_Compound_Conditional.py
@@ -1,8 +1,12 @@
 give_me_money = True
-fuel_my_car=True,
+fuel_my_car = True
 
-if (give_me_money or fuel_my_car) and not (give_me_money and fuel_my_car):
+if give_me_money or fuel_my_car:
     print('Thank you')
+elif give_me_money and fuel_my_car:
+    print('No you are too generous.')
+else:
+    print("I need your help")
 
 
 #eaten_peas = True
