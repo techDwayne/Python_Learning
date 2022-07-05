d60e960e730f4a8adfc845ea1c6c37ac255f8979
techD <ldsudduth@live.com>
2022-07-05 14:23:45 -0400

    commit

---
+++ ITProTV/Lesson11_Compound_Conditional.py
@@ -0,0 +1,15 @@
+give_me_money = True
+fuel_my_car=True,
+
+if (give_me_money or fuel_my_car) and not (give_me_money and fuel_my_car):
+    print('Thank you')
+
+
+#eaten_peas = True
+#eaten_carrots = True
+
+   
+#if eaten_peas and eaten_carrots: #Both conditions must be True
+#    print('You get ice cream!')
+#else:
+#    print('Nothing for you.')
