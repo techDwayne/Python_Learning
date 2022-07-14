4f6eee7baa0e2d62370aa8249e41e844b3092b81
techD <ldsudduth@live.com>
2022-07-14 09:14:53 -0400

    commit new

--- ITProTV/Lesson13_WhileLoop.py
+++ ITProTV/Lesson13_WhileLoop.py
@@ -1,4 +1,22 @@
-favorite_phrase='I like Cheese'
-for letter in favorite_phrase:
-    print(letter)
+#favorite_phrase='I like Cheese'
+#for letter in favorite_phrase:
+#    print(letter)
+#index=0
+#while index<len(favorite_phrase):
+#    letter=favorite_phrase[index]
+#    print(letter)
+#    index=index+1
+
+from math import fabs
+
+
+favorite_number = 7
+running=True
+while running:
+    guess=int(input("What is Vonne's favorite number?\n"))
+    if guess==favorite_number:
+        print('Yayyyy!!! You Win!!!!')
+        running=False
+    else:
+        print("Try Again!")
     
