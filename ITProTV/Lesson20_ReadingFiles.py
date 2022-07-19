594eb5edf8bc69718129b15f1246dd051ed999fb
techD <ldsudduth@live.com>
2022-07-19 07:41:37 -0400

    commit

--- ITProTV/Lesson20_ReadingFiles.py
+++ ITProTV/Lesson20_ReadingFiles.py
@@ -1,8 +1,8 @@
 
-path= '/GitHub/Python_Learning/ITProTV'
+path= '/GitHub/Python_Learning/ITProTV' #open path to file
 fname=input('File to read?\n')
 #file=open(f"{path}/{fname}")
-with open(f"{path}/{fname}") as file:
+with open(f"{path}/{fname}") as file: #context manager--will close file without explicit file.close()
     for line in file:
             print(line)
     #refill the cup
