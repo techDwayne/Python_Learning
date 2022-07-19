9abffef77473ad74f72c9219feaadafc5182c437
techD <ldsudduth@live.com>
2022-07-19 06:53:49 -0400

    commit

--- ITProTV/Lesson20_ReadingFiles.py
+++ ITProTV/Lesson20_ReadingFiles.py
@@ -1,9 +1,11 @@
 
-path='\GitHub\Python_Learning\ITProTV'
+path= '/GitHub/Python_Learning/ITProTV'
 fname=input('File to read?\n')
-file=open(f"{path}/{fname},'r'")
-#file=open("\data.txt",'r')
-for line in file:
+file=open(f"{path}/{fname}")
+lines=file.readlines()
+#file=open('data.txt','r')
+for line in lines:
         print(line)
-
+for line in lines:
+        print(line)
 file.close()
