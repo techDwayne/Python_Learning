9e262343ed386ea1cbda4521c4e09f5bdeaa2e91
techD <ldsudduth@live.com>
2022-07-19 09:58:44 -0400

    commit

--- ExperimentationFolder/CharCount.py
+++ ExperimentationFolder/CharCount.py
@@ -1,10 +1,13 @@
-
 path= '/GitHub/Python_Learning/ExperimentationFolder'
 fname=input('Enter file name: \n')
 list=open(f'{path}/{fname}')
 number_of_characters = 0
+for pl in list:
+    pl=pl.strip('\n')#strips newline char
+    print(pl)
+list=open(f'{path}/{fname}')
 for line in list:
-    line=line.strip('\n')
+    line=line.strip('\n')#strips newline char
     words=line.split()
     number_of_characters+=len(line)
 print('Characters: ', number_of_characters)
