460de91effe84895967c19a1511c3c6ccb1c545a
techD <ldsudduth@live.com>
2022-07-27 10:21:41 -0400

    commit

--- Py4E/ex_9.02.py
+++ Py4E/ex_9.02.py
@@ -4,5 +4,6 @@ counts=dict()
 for line in fhand:
     if line.startswith('From '):
         words=line.split()
+        print(words)
         counts[words[1]] = counts.get(words[1], 0)+1
 print(counts) 
