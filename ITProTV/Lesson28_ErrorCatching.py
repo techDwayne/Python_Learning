f46eafc7b6ef408ca72a76d24dfa2978615337ea
techD <ldsudduth@live.com>
2022-08-04 15:36:08 -0400

    commit

---
+++ ITProTV/Lesson28_ErrorCatching.py
@@ -0,0 +1,10 @@
+total=4
+count=0
+try:
+    avg=total/count
+    print("Average is: ", avg)
+except ZeroDivisionError as e:
+    print("Cannot divide by 0!", e)
+    
+
+print("Save my progress!!")
