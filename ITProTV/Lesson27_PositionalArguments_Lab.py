8fc32932b7a0a2c95fb564ec90be58d9061d459d
techD <ldsudduth@live.com>
2022-08-04 10:48:27 -0400

    commit

--- ITProTV/Lesson27_PositionalArguments_Lab.py
+++ ITProTV/Lesson27_PositionalArguments_Lab.py
@@ -1,5 +1,5 @@
 import csv
-with open ("data.csv","w", newline='') as csvFile:
+with open ("data2.csv","w", newline='') as csvFile:
     fieldnames = ['Name','City','State']
     writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
     writer.writeheader()
@@ -7,15 +7,15 @@ def myData(name,city,state):
     csvData = [name,city,state]
     writer = csv.writer(csvFile)
     writer.writerow(csvData)
-char = 'Y'
-while char == 'Y':
-    name = input("Enter Name:")
-    city = input("Enter City:")
-    state = input("Enter State:")
-    with open ("data.csv","a", newline='') as csvFile:
-        myData(name,city,state)
-        char = input("Do you want to continue adding records (Y/N):")
-filename = 'data.csv'
+
+name = input("Enter Name:")
+city = input("Enter City:")
+state = input("Enter State:")
+with open ("data2.csv","a", newline='') as csvFile:
+    myData(city=city, name=name, state=state)
+    myData(state=state,city=city,name=name)
+    myData(name=name,city=city,state=state)
+filename = 'data2.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     for row in reader:
