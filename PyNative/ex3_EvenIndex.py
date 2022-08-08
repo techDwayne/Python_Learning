45cf6fbdc68c4b37a19a2cb8d0218f59c5de410f
techD <ldsudduth@live.com>
2022-08-08 06:26:47 -0400

    commit

--- PyNative/ex3_EvenIndex.py
+++ PyNative/ex3_EvenIndex.py
@@ -1,8 +1,8 @@
-orig=input("Input? ")
-print("Original string is:", orig)
-#print(str1)
+origStr=input("Input? ")
+print("Original string is:", origStr)
+#print(OrigStr)
 
-str1=list(orig)
+str1=list(origStr)
 print(str1)
 print("Printing only even index chars")
 for i in str1[0::2]:
