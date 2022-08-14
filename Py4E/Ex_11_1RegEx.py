19c95e33faf852efdc9ac527725d6de87599b2d8
techD <ldsudduth@live.com>
2022-08-14 16:16:50 -0400

    comm

--- Py4E/Ex_11_1RegEx.py
+++ Py4E/Ex_11_1RegEx.py
@@ -1,22 +1,21 @@
 import re
 
-
 fhand=open('regex_sum_1317280.txt')
-number = []
-count = 0
+number = []#create empy list
+count = 0 #initiate count variable
 for line in fhand:
     line=line.rstrip()
-    x = re.findall('[0-9]+', line)
+    x = re.findall('[0-9]+', line) #find numbers in text file using regex
     
     if len(x)>0:
         print(x)
-        number=number + x
+        number=number + x #add number to list
 print(number)
-total = 0
-for item in number:
-    item=int(item)
+total = 0 #intitate total variable
+for item in number: #iterate through the list
+    item=int(item) #convert string in list to integer
     print(item)
-    total = total + item
-    count += 1
+    total = total + item #add integer to total variable until it reaches the end
+    count += 1 #increment count variable
 print(count)
 print(total)
