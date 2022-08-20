4d8781dc747fc96f506c8df177dfb8f84f679f8c
techD <ldsudduth@live.com>
2022-08-20 10:19:41 -0400

    commit

--- ExperimentationFolder/weather.py
+++ ExperimentationFolder/weather.py
@@ -13,21 +13,23 @@ prop = station['properties']
 grid_id = prop['gridId']
 grid_x = prop['gridX']
 grid_y = prop['gridY']
-json.dumps(station)
+
 
 
 # get the forecast
 url = f'https://api.weather.gov/gridpoints' \
       f'/{grid_id}/{grid_x},{grid_y}/forecast/hourly'
 fcast = requests.get(url)
-fcast = fcast.json()
 
 
-tdy = fcast['properties']['periods'][0]
-txt = tdy['shortForecast']
-temp_read=tdy['temperature']
+#tdy = fcast['properties']['periods'][0]
+#txt = tdy['shortForecast']
+#temp_read=tdy['temperature']
 
-print(temp_read)
-print(txt)
+#print(temp_read)
+#print(txt)
 
-
+fcast.raise_for_status()
+fcast_data=json.loads(fcast.text)
+weather=fcast_data['list']
+print(weather)
