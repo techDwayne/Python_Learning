50068181926009739e42c02f4daa26d8665f354f
techD <ldsudduth@live.com>
2022-08-22 14:35:39 -0400

    commit

--- ExperimentationFolder/weather.py
+++ ExperimentationFolder/weather.py
@@ -1,10 +1,18 @@
 import requests
 import json
+import geocoder
 
 # establish location
-LAT = 40.09768
-LNG = -76.72323
-
+loc = input("City, ST?" )
+ # pip install geocoder
+g = geocoder.osm( loc )
+results = g.json
+#print(json.dumps(results, indent=4))
+#print(results['lat'], results['lng'])
+LAT = results['lat']
+LNG = results['lng']
+print(LAT)
+print(LNG)
 
 # get station and grid ids
 url = f'https://api.weather.gov/points/{LAT},{LNG}'
@@ -21,17 +29,28 @@ url = f'https://api.weather.gov/gridpoints' \
 fcast = requests.get(url)
 fcast = fcast.json()
 
-print(json.dumps(fcast, indent=4))
 
-time = fcast['properties']['periods'][0]["startTime"]
-temp = fcast['properties']['periods'][0]['temperature']
-wndspd = fcast['properties']['periods'][0]['windSpeed']
-shrtFore = fcast['properties']['periods'][0]['shortForecast']
+#print(json.dumps(fcast, indent=4))
 
-print(time)
-print(temp)
-print(wndspd)
-print(shrtFore)
+#write to file
+with open('fcast.json', 'w') as f:
+      json.dump(fcast, f)
+
+#get objects from json
+hours = input("Number of hours of forecast? ")
+hrs=int(hours)
+#print(hrs)
+for i in range(0,hrs):
+      time = fcast['properties']['periods'][i]["startTime"]
+      temp = fcast['properties']['periods'][i]['temperature']
+      wndspd = fcast['properties']['periods'][i]['windSpeed']
+      shrtFore = fcast['properties']['periods'][i]['shortForecast']
+
+      print(time)
+      print(temp, 'F')
+      print(wndspd)
+      print(shrtFore)
+      print('')
 
 
 
