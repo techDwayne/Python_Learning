5098544566a1652db51f1f7b3502c3cd6bae6fae
techD <ldsudduth@live.com>
2022-08-30 12:01:58 -0400

    c

--- Kaggle/menu.py
+++ Kaggle/menu.py
@@ -2,8 +2,7 @@ num_customers = [137, 147, 135, 128, 170, 174, 165, 146, 126, 159,141, 148, 132,
 
 # TODO: Fill in values for the variables below
 avg_first_seven = sum(num_customers[:7])/7
-print(num_customers[:-7])
-avg_last_seven = sum(num_customers[:-7])
+avg_last_seven = sum(num_customers[:])
 max_month = max(num_customers)
 min_month = min(num_customers)
 print(avg_first_seven)
