b052883086451207c991848c2bba07d87e6d5e3c
techD <ldsudduth@live.com>
2022-09-01 08:54:08 -0400

    commit

--- Kaggle/MovieLike.py
+++ Kaggle/MovieLike.py
@@ -1,8 +1,9 @@
 def percentage_liked(ratings):
     list_liked = [i>=4 for i in ratings]
-    print(list_liked)
+    print(sum(percentage_liked[4:8]))/(sum(list_liked))
     # TODO: Complete the function
     return percentage_liked
 
 # Do not change: should return 0.5
 percentage_liked([1, 2, 3, 4, 5, 4, 5, 1])
+print(percentage_liked)
