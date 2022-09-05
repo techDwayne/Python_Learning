1eba68f61260039ba41397fb8169a798947c4a49
techD <ldsudduth@live.com>
2022-09-05 07:21:35 -0400

    c

---
+++ ExperimentationFolder/planets.py
@@ -0,0 +1,8 @@
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn','Uranus', 'Neptune']
+print(planets)
+planets.append('Pluto')
+print(planets)
+planets.sort()
+print(planets)
+planets.reverse()
+print(planets)
