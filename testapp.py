9cd4c43001a39496d546d98b879c1904cfdd497e
techD <ldsudduth@live.com>
2023-02-25 07:00:14 -0500

    commit

---
+++ testapp.py
@@ -0,0 +1,18 @@
+# print Hello World
+print("Hello World")
+
+#create a variable to hold a color
+my_variable = "blue"
+
+print(my_variable)
+
+#create an array to hold a list of colors
+my_array = ["red", "blue", "green"]
+
+#print the array
+
+print (my_array)
+
+#print the second item in the array
+print(my_array[1])
+
