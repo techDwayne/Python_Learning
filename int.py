e71ce19aa8590f43c3f3732478adde420d26d42b
techD <ldsudduth@live.com>
2023-04-28 08:21:11 -0400

    commit

---
+++ int.py
@@ -0,0 +1,18 @@
+# Write a function called is_ten that takes one parameter called x. The function must return True if x is 10 and False otherwise.
+
+def is_ten(x):
+    if x == 10:
+        return True
+    else:
+        return False
+    
+# Write a function named x that takes a string as its only parameter. The function should cast its input parameter to an integer and add 3 to it.
+
+def x(string):  
+    return int(string) + 3
+
+
+# Write a function named make_day_string that takes an integer named day and returns the string "it is day X of the month" where X is the value of the day param
+def make_day_string(day):
+    return f"it is day {day} of the month"
+
