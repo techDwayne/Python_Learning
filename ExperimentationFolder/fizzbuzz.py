2fd71fa6adbae29bf2a0cbc1fe9000b75136bc8d
techD <ldsudduth@live.com>
2023-07-10 08:57:39 -0400

    commit

---
+++ ExperimentationFolder/fizzbuzz.py
@@ -0,0 +1,18 @@
+
+#input: number
+#output: string
+#if number is divisible by 3, return "Fizz"
+# user input
+n = input("Number? ")
+num=int(n)
+
+def fizz_buzz(num):
+    if (num % 3 == 0) and (num % 5 == 0):
+        return "FizzBuzz"
+    if num % 3 == 0:
+        return "Fizz"
+    if num % 5 == 0:
+        return "Buzz"
+    return num
+print(fizz_buzz(num))
+
