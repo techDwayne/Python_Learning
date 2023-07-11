4df2088297567c7f1af22a4151988b32df69311b
techD <ldsudduth@live.com>
2023-07-11 11:56:33 -0400

    commit

--- ExperimentationFolder/virtenv.py
+++ ExperimentationFolder/virtenv.py
@@ -1,14 +1,11 @@
 import sys
 
 
-def get_base_prefix_compat():
-    """Get base/real prefix, or sys.prefix if there is none."""
-    return (
-        getattr(sys, "base_prefix", None)
-        or getattr(sys, "real_prefix", None)
-        or sys.prefix
-    )
+def is_venv():
+    return (hasattr(sys, 'real_prefix') or
+            (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix))
 
-
-def in_virtualenv():
-    return sys.prefix != get_base_prefix_compat()
+if is_venv():
+    print('inside virtualenv or venv')
+else:
+    print('outside virtualenv or venv')
