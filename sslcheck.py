47801c4d106339b60cd609241eb964d581c3fe6c
techD <ldsudduth@live.com>
2023-07-12 10:09:29 -0400

    updated to function

--- sslcheck.py
+++ sslcheck.py
@@ -1,14 +1,29 @@
-import requests
 import json
+import requests
 
 def check_ssl(url):
     try:
-        req = requests.get(url, verify=True)
-        print(url + ' has a valid SSL certificate!')
-    except requests.exceptions.SSLError:
-        print(url + ' has INVALID SSL certificate!')
+        response = requests.get(url,timeout=2)
+        if response.ok:
+            return response
+        else:
+            return None
+    except requests.exceptions.RequestException:
+        return None
 
-with open('urls.json') as json_file:
-    json_urls = json.load(json_file)
-    for url in json_urls:
-        check_ssl(url)
+def check_urls(json_file):
+    with open(json_file, 'r') as file:
+        urls = json.load(file)
+
+    for url in urls:
+        response = check_ssl(url)
+        if response is not None:
+            if response.status_code == 200:
+                print(f"{url} - Valid SSL certificate")
+            else:
+                print(f"{url} - Invalid SSL certificate")
+        else:
+            print(f"{url} - Unable to connect")
+
+json_file_path = '/home/lds/github/Python_Learning/urls.json'
+check_urls(json_file_path)
