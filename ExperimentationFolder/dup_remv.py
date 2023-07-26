12cbdc74f0b0eef6fbd0705bce5cf9c875b1a61e
techD <ldsudduth@live.com>
2023-07-26 10:54:12 -0400

    commit

--- ExperimentationFolder/dup_remv.py
+++ ExperimentationFolder/dup_remv.py
@@ -2,3 +2,5 @@ names = ["George", "Fred", "Alice", "Fred", "Sue", "Alice", "George", "George"]
 print("Before", names)
 names=list(set(names))
 print("After", names)
+names=sorted(set(names))
+print("Sorted", names)
